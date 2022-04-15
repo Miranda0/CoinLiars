fff0333caf4f397326dd776b79912e53a29a4fd8
Miranda <miranda.stoddard@mailfence.com>
2022-04-15 12:05:14 -0700

    initial commit

---
+++ Probability Calculator.py
@@ -0,0 +1,18 @@
+
+def cal_fair_chance():
+    return
+
+
+# The description of the game tells us the cheaters are using coins with a
+# 75% chance of heads
+def cal_cheat_chance():
+    return
+
+
+def main():
+    record_file = open("C:\\Users\\mirastod\\documents", 'w')
+    return
+
+
+if __name__ == 'main':
+    main()
